c709f09f9191bd25fe392d193c46764dc53d45a5
andrey <andrey.develop.a@gmail.com>
2024-03-07 02:10:59 +0700

    feat: profile

--- src/app/Models/User.php
+++ src/app/Models/User.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use Symfony\Component\HttpKernel\Profiler\Profile;
 
 class User extends Authenticatable
 {
@@ -25,5 +26,10 @@ class User extends Authenticatable
     public function findForPassport($username)
     {
         return $this->where("username", $username)->first();
+    }
+
+    public function profile()
+    {
+        return $this->hasOne(Profile::class);
     }
 }
