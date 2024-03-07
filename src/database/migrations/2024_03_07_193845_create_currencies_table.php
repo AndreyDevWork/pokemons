54d990b47428744e4454cad27f5d175bfb6126b8
andrey <andrey.develop.a@gmail.com>
2024-03-08 02:50:43 +0700

    feat: Currency table

---
+++ src/database/migrations/2024_03_07_193845_create_currencies_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create("currencies", function (Blueprint $table) {
+            $table->id();
+            $table->string("num_code");
+            $table->string("char_code");
+            $table->integer("nominal");
+            $table->string("name");
+            $table->decimal("value", 10, 4);
+            $table->decimal("vunit_rate", 10, 4);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists("currencies");
+    }
+};
