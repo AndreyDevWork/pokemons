421d58120148c70a2d8beb87806c5f05edc0567a
andrey <andrey.develop.a@gmail.com>
2024-03-23 00:40:46 +0700

    feat: swagger

--- src/app/Http/Resources/Profile/ProfileResource.php
+++ src/app/Http/Resources/Profile/ProfileResource.php
@@ -5,14 +5,47 @@ namespace App\Http\Resources\Profile;
 use App\Http\Resources\User\UserResource;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
+use OpenApi\Attributes as OA;
 
+#[
+    OA\Schema(
+        schema: "ProfileResource",
+        properties: [
+            "date_of_birth" => new OA\Property(
+                property: "date_of_birth",
+                type: "string",
+                example: "2001-02-14"
+            ),
+            "firstname" => new OA\Property(
+                property: "firstname",
+                type: "string",
+                example: "Slark"
+            ),
+            "lastname" => new OA\Property(
+                property: "lastname",
+                type: "string",
+                example: "Puchina"
+            ),
+            "role" => new OA\Property(
+                property: "role",
+                type: "string",
+                example: "student"
+            ),
+            "user" => new OA\Property(
+                property: "user",
+                ref: "#/components/schemas/UserResource",
+                type: "object"
+            ),
+        ]
+    )
+]
 class ProfileResource extends JsonResource
 {
     public function toArray(Request $request): array
     {
         return [
             "date_of_birth" => $this->date_of_birth,
-            "firstname" => $this->name,
+            "firstname" => $this->firstname,
             "lastname" => $this->lastname,
             "role" => $this->role->role,
             "user" => new UserResource($this->user),
