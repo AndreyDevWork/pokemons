1c86b0a180ca1555bbc8ef9c2bc31bfab7a69a51
andrey <andrey.develop.a@gmail.com>
2024-03-24 09:14:09 +0700

    fшч: status code

--- src/app/Http/Controllers/Auth/VerifyEmailController.php
+++ src/app/Http/Controllers/Auth/VerifyEmailController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Models\User;
 use Illuminate\Auth\Events\Verified;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 use Redirect;
 
 class VerifyEmailController extends Controller
@@ -13,7 +14,10 @@ class VerifyEmailController extends Controller
     public function sendEmailVerification(Request $request)
     {
         if ($request->user()->hasVerifiedEmail()) {
-            return response()->json(["error" => "Email already verified"]);
+            return response()->json(
+                ["error" => "Email already verified"],
+                Response::HTTP_CONFLICT
+            );
         }
 
         $request->user()->sendEmailVerificationNotification();
@@ -30,11 +34,17 @@ class VerifyEmailController extends Controller
                 sha1($user->getEmailForVerification())
             )
         ) {
-            return response()->json(["error" => "Invalid verification link"]);
+            return response()->json(
+                ["error" => "Invalid verification link"],
+                Response::HTTP_BAD_REQUEST
+            );
         }
 
         if ($user->hasVerifiedEmail()) {
-            return response()->json(["error" => "Email already verified"]);
+            return response()->json(
+                ["error" => "Email already verified"],
+                Response::HTTP_CONFLICT
+            );
         }
 
         if ($user->markEmailAsVerified()) {
