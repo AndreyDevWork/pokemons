0547f750accbb306c511b0bc3addb5dbac4ef466
andrey <andrey.develop.a@gmail.com>
2024-03-24 08:04:18 +0700

    feat: Email virified listenner and message

---
+++ src/app/Listeners/EmailVerifiedListener.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Listeners;
+
+use App\Mail\EmailVerifiedMail;
+use Illuminate\Auth\Events\Verified;
+use Mail;
+
+class EmailVerifiedListener
+{
+    /**
+     * Create the event listener.
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Handle the event.
+     */
+    public function handle(Verified $event): void
+    {
+        Mail::to("ohotaandrey4@gmail.com")->send(new EmailVerifiedMail());
+    }
+}
